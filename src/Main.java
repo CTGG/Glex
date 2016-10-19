6403ef749badede1558ee2b8150c1cd023c50559
CTGG <ctg14@software.nju.edu.cn>
2016-10-19 20:38:57 +0800

    part1 IO
    IO

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Main {
+	public static void main(String[] args) {
+		//put input lines into a list
+		ArrayList<String> list = IOHelper.readFile("/input.txt");
+		//for each list check char by char
+		for (String str : list) {
+			
+		}
+		
+	}
+}
